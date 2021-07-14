6bf1f3f858689e964281ebbd2adfec2599908755
Yohann MARTZOLFF <50139430+MarDi66@users.noreply.github.com>
2021-07-14 11:25:38 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import './App.css';
+import { Error } from './classes/Error';
+import { GitHubUser } from './classes/GitHubUser';
+import NoUserFound from './components/NoUserFound';
+import UserCard from './components/UserCard';
 
 function App() {
+
+  // State local
+  const [ nameInput, setNameInput ] = useState<string>('')
+  const [ usersList, setUsersList ] = useState<Array<GitHubUser>>()
+  const [ error, setError ] = useState<Error>(new Error())
+
+
+  useEffect(() => {
+
+    // Timeout pour la recherche automatique, permet de ne pas trigger à chaque input
+    const delaySearchFn = setTimeout(() => {
+      
+      fetch(`https://api.github.com/search/users?q=${ nameInput }`)
+      .then(res => res.json())
+      .then(res => setUsersList(res.items))
+      .catch(() => setError({ error: true, message: 'OOPS ! Seems like there\'s something wrong...' }))
+
+    }, 500)
+
+    return () => clearTimeout(delaySearchFn)
+
+  }, [nameInput])
+
+  
+  // --- HANDLERS --- //
+  // Changement de value de l'input de recherche
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => setNameInput(e.target.value)
+
+
+  // Constantes
+  const usersDisplay = usersList?.length === 0 ? <NoUserFound userName={ nameInput } /> : usersList?.map(user => <UserCard key={ user.id } user={ user } />)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>Github Users Search Engine</h1>
+
+      { 
+        !error.error &&
+          <>
+            <input autoFocus type='text' value={ nameInput } onChange={ handleChangeInput } />
+
+            <div className='users_list_container'>
+              { usersDisplay }
+            </div>
+          </>
+      }
+
+      { 
+        error.error &&
+          <div>{ error.message }</div>
+      }
+      
     </div>
   );
 }
